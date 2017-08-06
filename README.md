bd4dc61e9ea61569a7736062ba11ffc6422eb540
masha <mashavityuk2@gmail.com>
2017-08-06 23:53:07 +0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Installation
+
+1. npm install -g gulp
+2. npm install
+3. gulp
---
+++ assets/js/common.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function(){
+
+    $('.aside-menu .item-head').on('click',function () {
+        $(this).parents('.aside-menu-i').toggleClass('open');
+    });
+    $('.main-block-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+         // arrows: false,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next')
+    });
+    $('.login-button').on('click', function () {
+        $('.login-popup').addClass('open')
+    });
+
+    $('.login-popup .overlay').on('click', function () {
+        $('.login-popup').removeClass('open')
+    });
+
+
+    $('.mobile-menu-button').on('click', function () {
+        $('.mobile-menu-button').toggleClass('open')
+        $('.menu-nav').toggleClass('open')
+    });
+
+});
+
+
